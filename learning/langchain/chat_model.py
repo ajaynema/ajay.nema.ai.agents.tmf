2d2ca3175f92b2648af8e4b642f076c377e5442e
ajaynema <ajay.nema@gmail.com>
2025-01-23 19:11:47 +0530

    chat model

--- learning/langchain/chat_model.py
+++ learning/langchain/chat_model.py
@@ -1,1 +1,20 @@
-print("test")
+from dotenv import load_dotenv
+from langchain_ollama import ChatOllama
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+import os
+
+load_dotenv()
+
+chat_history = []
+chat_history.append(SystemMessage("you are a math expert"))
+
+llm = ChatOllama(model="gemma2")
+while True :
+    query = input("You : ")
+    if query.lower() == "exit":
+        break
+    chat_history.append(HumanMessage(content=query))
+
+    result = llm.invoke(chat_history)
+    chat_history.append(AIMessage(content=result.content))
+    print(result.content)
